9788875b17163e6add4d5b1a2b6b3a9b2bdabc6f
Roman <cvqsrsxl@gmail.com>
2025-04-23 15:47:07 +0300

    evenmoreminorchanges

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ def get_data():
     params = []
 
     if time_filter != None:
-        query += " WHERE timestamp >= ?"
+        query += " WHERE timestamp <= ?"
         params.append(time_filter.strftime('%d.%m.%Y'))
         
     if city_filter:
